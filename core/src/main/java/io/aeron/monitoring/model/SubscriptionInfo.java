71c698190f7fddcec3d0a000649bb903bb13e5e3
denis <denisabr@bk.ru>
2018-10-07 17:08:43 +0300

    Implement draft version of the REST API including swagger which provides documentation and test application to verify how the API works

--- core/src/main/java/io/aeron/monitoring/model/SubscriptionInfo.java
+++ core/src/main/java/io/aeron/monitoring/model/SubscriptionInfo.java
@@ -1,9 +1,17 @@
 package io.aeron.monitoring.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
 public class SubscriptionInfo {
 
-
+    @ApiModelProperty("Subscription ID")
     private final Integer id;
+
+    @ApiModelProperty("The position an individual Subscriber has reached on a " +
+            "session-channel-stream tuple. It is possible to have multiple Subscribers on the " +
+            "same machine tracked by a MediaDriver")
     private long position;
 
     public SubscriptionInfo(final Integer id) {
